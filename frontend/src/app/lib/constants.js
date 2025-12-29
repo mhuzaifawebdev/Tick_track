5129312a044d9e76e95ed6d84c352a4313db6729
MhuzaifaWebDev <mhuzaifawebdev@gmail.com>
2025-12-29 21:39:35 +0500

    constants updated

--- frontend/src/app/lib/constants.js
+++ frontend/src/app/lib/constants.js
@@ -1,6 +1,6 @@
 // API Base URL
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3003";
+  process.env.NEXT_PUBLIC_BACKEND_URL ;
 
 // API Endpoints
 export const API_ENDPOINTS = {
